f5cef0a471cc61d0d4b9256f44ee8e54a24166c6
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-20 06:49:55 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -11,8 +11,6 @@ class AppointmentsController < ApplicationController
 
   def create
     @appointment = Appointment.create(appointment_params)
-    @appointment.doctor = Doctor.find_or_create_by(name: name)
-    @appointment.patient = Patient.find_or_create_by()
 
     if @appointment.save
       redirect_to @appointment
